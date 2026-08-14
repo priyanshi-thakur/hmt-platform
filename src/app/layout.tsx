7a073a692c295b87adaefb0ca9d7572569807812
Priyanshi Thakur <232033269+priyanshi-thakur@users.noreply.github.com>
2026-08-14 22:29:25 +0530

    Add metadata for HMT Platform in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: 'HMT Platform',
+  description: 'Hackathon Management Tool',
+}
+
 export default function RootLayout({
   children,
 }: {
